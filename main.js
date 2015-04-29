c71d0e7a3ff36a43aa425bfec417b80b340be36d
Eric Martinez <martinez_eric82@yahoo.com>
2015-04-29 09:05:37 -0700

    my week two assessment made button work

---
+++ main.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+	$( '.dan' ).click(function() {
+      alert( "A button with the alert class was clicked!" );
+  });
+});
+
+$( document ).ready(function() {
+	$( '#iwao' ).click(function() {
+      alert( "A button with the alert class was clicked!" );
+  });
+});
